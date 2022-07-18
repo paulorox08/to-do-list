20c13ab388cb452022212a29c1f48a28c8a3e0ff
Paulo Adeva <paulo.adeva@gmail.com>
2022-07-18 23:26:29 +1000

    added animations

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import './style.css';
 let myProjects = [];
 let myList = [];
 
+function createTitle() {
+    const container = document.querySelector('.container');
+
+    const name = document.createElement('div');
+    name.className = "name";
+    name.innerText = ".list";
+
+    container.appendChild(name);
+};
+
+createTitle();
+
+function addProject() {
+    const container = document.querySelector('.container');
+
+    const addProject = document.createElement('div');
+    addProject.className = "addProject";
+    addProject.innerText = "Add Project";
+
+    container.appendChild(addProject);
+};
+
+addProject();
